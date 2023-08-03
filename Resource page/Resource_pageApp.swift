a46b1c15b6f5640c65afe55173652d86fffce17a
Emily Duong <emilykduong@gmail.com>
2023-08-03 11:47:43 -0700

    Initial Commit

---
+++ Resource page/Resource_pageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Resource_pageApp.swift
+//  Resource page
+//
+//  Created by Cindy Kwok on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Resource_pageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
